e89bbb016bc7a88ee56f8da672dab3365f2e4a4e
Tejas Jadhav <tjadhav95@gmail.com>
2024-12-07 15:52:10 +0530

    Day7 Part2

--- Day7/index.js
+++ Day7/index.js
@@ -3,53 +3,79 @@ import { data } from "./data.js";
 const show = console.log;
 
 const input = data
-    .trim()
-    .split("\n")
-    .map(line => {
-        const [ans, itemStr] = line.split(":").map(x => x.trim());
-        const items = itemStr.split(" ").map(Number);
-        return { ans: Number(ans), items };
-    });
+  .trim()
+  .split("\n")
+  .map((line) => {
+    const [ans, itemStr] = line.split(":").map((x) => x.trim());
+    const items = itemStr.split(" ").map(Number);
+    return { ans: Number(ans), items };
+  });
 
-const opsDict = {};
-function getOpsList(items) {
-    const opsListLength = items.length - 1;
-    if (opsDict[opsListLength]) return opsDict[opsListLength];
+let opsDict = {};
+function getOpsList(items, isPart2) {
+  const opsListLength = items.length - 1;
+  if (opsDict[opsListLength]) return opsDict[opsListLength];
 
-    const opsList = [];
-    function recurs(ops, index) {
-        if (index === opsListLength) {
-            opsList.push([...ops]);
-            return;
-        }
-
-        recurs([...ops, "+"], index + 1);
-        recurs([...ops, "*"], index + 1);
+  const opsList = [];
+  function recurs(ops, index) {
+    if (index === opsListLength) {
+      opsList.push([...ops]);
+      return;
     }
 
-    recurs([], 0);
-    opsDict[opsListLength] = opsList;
-    return opsList;
+    recurs([...ops, "+"], index + 1);
+    recurs([...ops, "*"], index + 1);
+    if (isPart2) recurs([...ops, "|"], index + 1);
+  }
+
+  recurs([], 0);
+  opsDict[opsListLength] = opsList;
+  return opsList;
 }
 
 const performOps = (items, ops) => {
-    return items.reduce((ans, item, index) => {
-        if (index === 0) return item;
-        const op = ops[index - 1];
-        return op === "+" ? ans + item : ans * item;
-    }, 0);
+  return items.reduce((ans, item, index) => {
+    if (index === 0) return item;
+    const op = ops[index - 1];
+    return op === "+"
+      ? ans + item
+      : op === "*"
+      ? ans * item
+      : Number(`${ans}${item}`);
+  }, 0);
 };
 
-let part1 = 0;
-input.forEach(({ ans, items }) => {
+const part1 = () => {
+  opsDict = {};
+  let output = 0;
+  input.forEach(({ ans, items }) => {
     const opsList = getOpsList(items);
     for (let i = 0; i < opsList.length; i++) {
-        const ops = opsList[i];
-        if (ans === performOps(items, ops)) {
-            part1 += ans;
-            break;
-        }
+      const ops = opsList[i];
+      if (ans === performOps(items, ops)) {
+        output += ans;
+        break;
+      }
     }
-});
+  });
+  return output;
+};
 
-show(part1); // 3119088655389
+const part2 = () => {
+  opsDict = {};
+  let output = 0;
+  input.forEach(({ ans, items }) => {
+    const opsList = getOpsList(items, true);
+    for (let i = 0; i < opsList.length; i++) {
+      const ops = opsList[i];
+      if (ans === performOps(items, ops)) {
+        output += ans;
+        break;
+      }
+    }
+  });
+  return output;
+};
+
+show(part1()); // 3119088655389
+show(part2()); // 264184041398847
