376900fb9aa4f4e1b9e85281e3ced0c9c046efe4
Tejas Jadhav <tjadhav95@gmail.com>
2024-12-15 16:38:04 +0530

    Day 15 Part 1

---
+++ Day15/index.js
@@ -0,0 +1,74 @@
+import { getGrid, getMoves } from "./data.js";
+const print = console.log;
+const printGrid = (grid) => print(grid.map((l) => l.join("")));
+
+const grid = getGrid()
+  .split("\n")
+  .map((line) => line.split(""));
+const moves = getMoves().split("\n").join("").split("");
+
+const BOX = "O";
+const ROBOT = "@";
+const EMPTY = ".";
+const WALL = "#";
+const MOVEMENT = {
+  "^": [-1, 0],
+  ">": [0, 1],
+  v: [1, 0],
+  "<": [0, -1],
+};
+
+const getStart = () => {
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (grid[row][col] === ROBOT) return { row, col };
+    }
+  }
+};
+const start = getStart();
+
+let curr = { ...start };
+moves.forEach((move) => {
+  const delta = MOVEMENT[move];
+  const next = { row: curr.row + delta[0], col: curr.col + delta[1] };
+  if (grid[next.row][next.col] === WALL) {
+    return;
+  }
+
+  const moveBox = (row, col) => {
+    if (grid[row][col] === EMPTY) {
+      return true;
+    }
+    if (grid[row][col] === BOX) {
+      const boxMoved = moveBox(row + delta[0], col + delta[1]);
+      if (boxMoved) {
+        grid[row + delta[0]][col + delta[1]] = BOX;
+        grid[row][col] = EMPTY;
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (grid[next.row][next.col] === BOX) moveBox(next.row, next.col);
+
+  if (grid[next.row][next.col] === EMPTY) {
+    grid[next.row][next.col] = ROBOT;
+    grid[curr.row][curr.col] = EMPTY;
+    curr = { ...next };
+    return;
+  }
+});
+
+// printGrid(grid);
+
+let part1 = 0;
+grid.forEach((row, rowIndex) => {
+  row.forEach((item, colIndex) => {
+    if (item === BOX) {
+      part1 += 100 * rowIndex + colIndex;
+    }
+  });
+});
+
+print(part1); // 1552463
